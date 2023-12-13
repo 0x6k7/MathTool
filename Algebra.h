3c146cb725e4a8edd39375908ea3662cf60d20c4
0x6k7 0x6k7 <s0x6k7@gmail.com>
2023-12-13 15:35:07 +0200

    test

--- Algebra.h
+++ Algebra.h
@@ -4,6 +4,8 @@
 using namespace std;
 
 void calculator(float, float);
+void solveLinearEquation(float, float);
+void solveQuadraticEquation(float, float, float);
 
 void calculator(float number1, float number2) {
 	system("cls");
@@ -27,4 +29,41 @@ void calculator(float number1, float number2) {
 		cout << number1 << "/" << number2 << "=" << division;
 	else
 		cout << "Error: Division by Zero" << endl;
+}
+
+void solveLinearEquation(float x1, float number) {
+	system("cls");
+
+	float x = x1 / number;
+
+	if (number == 0 && x == 0)
+		cout << "The equation has infinite solutions" << endl;
+	else if (number == 0)
+		cout << "The equation has no solutions" << endl;
+	else {
+		cout << number << "x" << "=" << x1 << endl;
+		cout << "x=" << x1 << endl;
+	}
+}
+
+void solveQuadraticEquation(float a, float b, float c) {
+	system("cls");
+
+	float discriminant = pow(b, 2) - 4 * a * c;
+
+	if (discriminant > 0) {
+		float x_1 = (-b + sqrt(discriminant)) / (2 * a);
+		float x_2 = (-b - sqrt(discriminant)) / (2 * a);
+
+		cout << "x_1=" << x_1 << endl;
+		cout << "x_2=" << x_2 << endl;
+	}
+
+	else if (discriminant == 0) {
+		float x_1 = (-b) / (2 * a);
+		cout << "x_1=" << x_1 << endl;
+	}
+
+	else
+		cout << "The equation has no solutions" << endl;
 }
--- MathTool.cpp
+++ MathTool.cpp
@@ -14,8 +14,19 @@ int main() {
 
     cout << "Algebra Menu. \n";
     cout << "1. Calculator" << endl;
+    cout << "2. Equations" << endl;
 
-    switch (choice) {
+    int choice2;
+    cout << "Enter choice: ";
+    cin >> choice2;
+
+    if (choice2 == 2) {
+        cout << "Equations Menu. \n";
+        cout << "1. Linear Equations" << endl;
+        cout << "2. Quadratic Equations" << endl;
+    }
+
+    switch (choice2) {
         case 1:
             int choice2;
             cout << "Enter choice: ";
@@ -31,7 +42,40 @@ int main() {
 
                 calculator(number, number2);
             }
+
+            if (choice2 == 2) {
+                float number, x;
+                cout << "Enter number: ";
+                cin >> number;
+
+                cout << "Enter x: ";
+                cin >> x;
+
+                solveLinearEquation(number, x);
+            }
             break;
+        case 2:
+            int choice;
+            cout << "Enter choice: ";
+            cin >> choice;
+
+            if (choice == 2) {
+                float a, b, c;
+                cout << "Enter a: ";
+                cin >> a;
+
+                cout << "Enter b: ";
+                cin >> b;
+
+                cout << "Enter c: ";
+                cin >> c;
+
+                if (a == 0)
+                    solveLinearEquation(c, b);
+                else
+                    solveQuadraticEquation(a, b, c);
+                break;
+            }
         default:
             cout << "Invalid choice" << endl;
     }
